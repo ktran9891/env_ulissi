6b5a1279568d14c93203ff51846af4c0a1f5f5a3
Kevin Tran <ktrain9891@gmail.com>
2020-07-23 13:49:03 -0400

    Add GASpy to SSH path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,21 @@
 #!su ktran /bin/bash
 
+
+########## Begin user-specific configurations ##########
+
+# Load environment
+source /home/ktran/miniconda3/bin/activate
+
+# Set wandb parameters
+export WANDB_CONFIG_DIR="$HOME/.config/wandb"
+export WANDB_API_KEY="577458cd7eb09405c7a88fbd43067723dae8c154"
+
+# Finish installing GASpy
+export PYTHONPATH="$HOME/GASpy/GASpy_regressions:${PYTHONPATH}"
+export PYTHONPATH="$HOME/GASpy:${PYTHONPATH}"
+
+########## End user-specific configurations ##########
+
 if [ ! -d "~/.ssh/" ] 
 then 
     ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
